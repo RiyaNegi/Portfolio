f8378d8eb9fcf7c4d26b9dcd19da892a1aab7de6
Riya Negi <riyanegi221b@gmail.com>
2024-06-14 23:13:02 +0530

    Add initial structure and configuration

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b6250bbdee57c51"; }
+/******/ 		__webpack_require__.h = function() { return "666e104dc9739abe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
