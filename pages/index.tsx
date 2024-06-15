fe516c54af5bd8b840d0afdac812d8e8e1e32a82
Riya Negi <riyanegi221b@gmail.com>
2024-06-15 17:46:44 +0530

    add theme toggle button and introduce basic framer motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
+
+import Section from '../components/section'
 
 const Page = () => {
   return (
     <Container>
       <Box
         borderRadius="lg"
-        bg="aliceblue"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
         p={3}
         mb={6}
         mt={12}
@@ -21,7 +29,38 @@ const Page = () => {
           </Heading>
           <span>Keen engineer making inutitive websites </span>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="200px"
+            h="200px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/images/Riya.jpeg"
+              alt="Profile image"
+              width="200"
+              height="200"
+            />
+          </Box>
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        {' '}
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
