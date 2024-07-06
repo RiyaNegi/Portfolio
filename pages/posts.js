63c3d3d5656622f45cdf3205e29f369e475deb5b
Riya Negi <riyanegi221b@gmail.com>
2024-07-06 20:26:07 +0530

    Add posts data

--- pages/posts.js
+++ pages/posts.js
@@ -2,24 +2,54 @@ import { Box, Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
 import Layout from '../components/layouts/article'
 import { GridItem } from '../components/grid-item'
-import random from '../public/images/bee.png'
+import webpack from '../public/images/Posts/webpack.png'
+import tsReact from '../public/images/Posts/tsReact.png'
+import scalable from '../public/images/Posts/scalable.png'
+import metaTags from '../public/images/Posts/metaTags.png'
 
 const Posts = () => {
   return (
     <Layout title="Posts">
-      <Container>
+      <Container mt={50}>
         <Heading as="h4" fontSize={20} mb={4}>
-          Popular POsts
+          Posts
         </Heading>
-        <SimpleGrid columns={[1, 1, 2]}>
+        <SimpleGrid columns={[1, 1, 2]} gap={8}>
           <Section>
             <GridItem
-              title="NPM react-comments-section"
-              href={'/posts'}
-              thumbnail={random}
-            >
-              hello
-            </GridItem>
+              title="Setting up Webpack 5 with React and Babel from scratch"
+              href={
+                'https://dev.to/riyanegi/setting-up-webpack-5-with-react-and-babel-from-scratch-2021-271l'
+              }
+              thumbnail={webpack}
+            ></GridItem>
+          </Section>
+          <Section>
+            <GridItem
+              title="Getting started with Typescript with React Hooks"
+              href={
+                'https://dev.to/riyanegi/getting-started-with-typescript-with-react-hooks-2021-5cfd'
+              }
+              thumbnail={tsReact}
+            ></GridItem>
+          </Section>
+          <Section>
+            <GridItem
+              title="Dummy guide to building scalable react web architecture"
+              href={
+                'https://dev.to/riyanegi/dummy-guide-to-building-scalable-react-web-architecture-53dc'
+              }
+              thumbnail={scalable}
+            ></GridItem>
+          </Section>
+          <Section>
+            <GridItem
+              title="Solving Issues with og: Meta Tags: A Comprehensive Guide"
+              href={
+                'https://dev.to/riyanegi/solving-issues-with-og-meta-tags-a-comprehensive-guide-22c2'
+              }
+              thumbnail={metaTags}
+            ></GridItem>
           </Section>
         </SimpleGrid>
       </Container>
--- pages/works.js
+++ pages/works.js
@@ -14,7 +14,7 @@ const Works = () => {
       <Heading as="h3" fontSize={20} mb={4}>
         Works
       </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+      <SimpleGrid columns={[1, 1, 2]} gap={10}>
         <Section>
           <WorkGridItem
             id="npm-react-comments-section"
