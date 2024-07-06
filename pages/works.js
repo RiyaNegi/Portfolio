0201ce901d4e50896f3974ff498c2b47a22c12f9
Riya Negi <riyanegi221b@gmail.com>
2024-07-06 19:32:49 +0530

    add work section details

--- pages/works.js
+++ pages/works.js
@@ -1,7 +1,12 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
-import random from '../public/images/bee.png'
+import kanban from '../public/images/work/kanban.png'
+import comments from '../public/images/work/comments.png'
+import student from '../public/images/work/student.png'
+import wiredClan from '../public/images/work/wiredClan.png'
+import social from '../public/images/work/social.png'
+import teerex from '../public/images/work/teerex.png'
 
 const Works = () => {
   return (
@@ -14,16 +19,47 @@ const Works = () => {
           <WorkGridItem
             id="npm-react-comments-section"
             title="NPM react-comments-section"
-            thumbnail={random}
+            thumbnail={comments}
           >
-            A Markdown note-taking app with 100+ plugins, cross-platform and
-            encrypted data sync support
+            npm package for integrating a fully functional comments section in
+            react web apps along with custom input fields.
           </WorkGridItem>
         </Section>
         <Section>
-          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={random}>
-            A Markdown note-taking app with 100+ plugins, cross-platform and
-            encrypted data sync support
+          <WorkGridItem
+            id="social"
+            title="Social-Monitoring"
+            thumbnail={social}
+          >
+            An application where we are able to utilize data for data
+            interpretations and visualizations such as Outreach prediction, word
+            cloud, sentiment analysis and brand mentions.
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem id="wiredclan" title="WiredClan" thumbnail={wiredClan}>
+            A social website built exclusively for college students to showcase
+            their projects, host hackathons. Built on the PERN stack.
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem id="kanban" title="Kanban Board" thumbnail={kanban}>
+            Kanban board along with a simple todo list view for better task
+            management. The Board view and List view are interconnected and any
+            changes made on either of the views will be persist.
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem id="teerex" title="Teerex-store" thumbnail={teerex}>
+            A mock e-commerce clothing website with the abilities to filter and
+            search through the prodcuts
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem id="student" title="Student-Data" thumbnail={student}>
+            A single page react application where one side of the page has a
+            student details form to submit and the other side has a table that
+            displays all the registered students along with their details.
           </WorkGridItem>
         </Section>
       </SimpleGrid>
